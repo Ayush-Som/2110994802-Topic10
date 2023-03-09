abde13a1b4690dfe964c1298e40b495723094054
Ayush-Som <103960059+Ayush-Som@users.noreply.github.com>
2023-03-09 13:33:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send('hello world');
-});
-
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
+});
+
+app.get("*", (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.send("Hello world");
 });
